91cd7b8c7151a7fb7096bcaec8dc5d3e8bf5ca5e
plutoattack <plutoattackme@gmail.com>
2014-11-08 22:25:27 +0200

    OrgPosition Domain Created

---
+++ grails-app/domain/com/meerkat/OrgPosition.groovy
@@ -0,0 +1,8 @@
+package com.meerkat
+
+class OrgPosition {
+    String label
+    static constraints = {
+        label(blank: false)
+    }
+}
--- grails-app/domain/com/meerkat/Signer.groovy
+++ grails-app/domain/com/meerkat/Signer.groovy
@@ -1,14 +1,12 @@
 package com.meerkat
 
-import org.apache.maven.model.Organization
-
 class Signer {
     String firstName
     String lastName
     boolean active
     Organization org
     boolean hasOrganizatioSignRights
-
+    OrgPosition position
     static constraints = {
 
     }
