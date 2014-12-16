2d219040850d19c77c48df4d70f3a0e1cfbac2be
spirosbond <spirosbond@hotmail.com>
2014-12-17 00:15:10 +0100

    implemented search function to perform "dummy" search and show the results

--- grails-app/controllers/com/meerkat/SearchController.groovy
+++ grails-app/controllers/com/meerkat/SearchController.groovy
@@ -2,5 +2,34 @@ package com.meerkat
 
 class SearchController {
 
+    def showSearchResults(List<Decision> decisionList, List<Signer> signerList, List<Type> typeList, List<Geo> geoList) {
+
+        if (params.clicked == null) {
+            if (decisionList == null) decisionList = Decision.list(params)
+            if (signerList == null) signerList = Signer.list(params)
+            if (typeList == null) typeList = Type.list(params)
+            if (geoList == null) geoList = Geo.list(params)
+        }
+        if (params.clicked == "decision") {
+            geoList = Geo.list(params)
+        } else if (params.clicked == "signer") {
+            geoList = Geo.list(params)
+        } else if (params.clicked == "type") {
+            geoList = Geo.list(params)
+        } else if (params.clicked == "geo") {
+            geoList = Geo.list(params)
+        }
+
+        render(template: 'search_results', model: ['decisionList': decisionList, 'signerList': signerList, 'typeList': typeList, 'geoList': geoList])
+    }
+
+    def searchFromBar() {
+
+        print(params)
+        //TODO: Call service for search
+        showSearchResults(null, null, null, null)
+
+    }
+
     def index() {}
 }
