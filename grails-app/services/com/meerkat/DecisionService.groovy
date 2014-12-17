9a2950b34bce1354038a5c0aa484bd0cfe18a032
plutoattack <plutoattackme@gmail.com>
2014-12-17 21:26:06 +0200

    all service actions turned to static

--- grails-app/services/com/meerkat/DecisionService.groovy
+++ grails-app/services/com/meerkat/DecisionService.groovy
@@ -4,22 +4,22 @@ import grails.transaction.Transactional
 
 @Transactional
 class DecisionService {
-    /*
+    /**
     Return the decision identified by ada with all the version history
     parameters: String ada
     return: List decisions
      */
-    def getDecisionByAda(String ada){
+    static def getDecisionByAda(String ada){
         def decisions=Decision.findAllByAda(ada)
         return decisions
     }
 
-    /*
+    /**
     Return the only the correct decision based on the given ada
     parameters: String ada
     return: Decision decisions
      */
-    def getCorrectDecisionByAda(String ada){
+    static def getCorrectDecisionByAda(String ada){
         def decisions=Decision.findAllByAda(ada)
         def returnDecision
         decisions.each {d->
@@ -30,12 +30,12 @@ class DecisionService {
         return returnDecision
     }
 
-    /*
+    /**
     Find decisions with exact or like Protocol Number as given
     parameters: String number,the 4 params for pagination
     return:List decisions
      */
-    def findDecisionByProtNum(String number,int max,int offset,String sort,String orderList){
+    static def findDecisionByProtNum(String number,int max,int offset,String sort,String orderList){
         def decisions=Decision.createCriteria().list() {
             like("protocolNumber",number)
             order(sort,orderList)
@@ -45,12 +45,12 @@ class DecisionService {
         return decisions
     }
 
-    /*
+    /**
     Find decisions with subject like the param given
     parameters: String subject,the 4 params for pagination
     return:List decisions
      */
-    def findDecisionBySubject(String subject,int max,int offset,String sort,String orderList){
+    static def findDecisionBySubject(String subject,int max,int offset,String sort,String orderList){
         def decisions=Decision.createCriteria().list() {
             like("subject","%"+subject+"%")
             order(sort,orderList)
@@ -60,12 +60,12 @@ class DecisionService {
         return decisions
     }
 
-    /*
+    /**
     Find decisions with type exactly as the param given
     parameters: String type,the 4 params for pagination
     return:List decisions
      */
-    def findDecisionByType(String type,int max,int offset,String sort,String orderList){
+    static def findDecisionByType(String type,int max,int offset,String sort,String orderList){
         def decisions=Decision.createCriteria().list() {
             eq("type",type)
             order(sort,orderList)
@@ -75,12 +75,12 @@ class DecisionService {
         return decisions
     }
 
-    /*
+    /**
     Find decisions with geo exactly as the param given
     parameters: Geo geo,the 4 params for pagination
     return:List decisions
      */
-    def findDecisionByPOI(Geo geo,int max,int offset,String sort,String orderList){
+    static def findDecisionByPOI(Geo geo,int max,int offset,String sort,String orderList){
         def decisions=Decision.createCriteria().list {
             eq("geo",geo)
             order(sort,orderList)
@@ -90,14 +90,14 @@ class DecisionService {
         return decisions
     }
 
-    /*
-    Find decisions with signer like or exactly the param given
+    /**
+    Find decisions signed by signer like or exactly as the param given
     parameters: String signer,the 4 params for pagination
     return:List decisions
      */
-    def findDecisionBySigner(String signer,int max,int offset,String sort,String orderList){
+    static def findDecisionBySigner(String signer,int max,int offset,String sort,String orderList){
         def decisions=Decision.createCriteria().list() {
-            like("subject",signer)
+            like("signer",signer)
             order(sort,orderList)
             firstResult(offset)
             maxResults(max)
@@ -105,12 +105,12 @@ class DecisionService {
         return decisions
     }
 
-    /*
-    Find decisions with unit like or exactly the param given
+    /**
+    Find decisions taken by unit like or exactly as the param given
     parameters: String subject,the 4 params for pagination
     return:List decisions
      */
-    def findDecisionByUnit(String unit,int max,int offset,String sort,String orderList){
+    static def findDecisionByUnit(String unit,int max,int offset,String sort,String orderList){
         def decisions=Decision.createCriteria().list() {
             like("subject",unit)
             order(sort,orderList)
@@ -120,12 +120,12 @@ class DecisionService {
         return decisions
     }
 
-    /*
+    /**
     Returns the thematic category of a decision
     parameters: Decision decision
     return: List themCat
      */
-    def getThematicCategory(Decision decision){
+    static def getThematicCategory(Decision decision){
         def themCat=DictionaryItem.createCriteria().list{
             and{
                 eq("ada",decision.ada)
@@ -135,12 +135,12 @@ class DecisionService {
         return themCat
     }
 
-    /*
+    /**
     return a map containing label of extra field and value of the given as parameter decision
     parameters: Decision decision
     return: Map returnMap
      */
-    def getExtrafields(Decision decision){
+    static def getExtraFields(Decision decision){
         def returnMap=[:]
         def extras=DecisionExtraFieldValue.createCriteria().list {
             eq("ada",decision.ada)
@@ -152,4 +152,6 @@ class DecisionService {
         }
         return returnMap
     }
+
+
 }
