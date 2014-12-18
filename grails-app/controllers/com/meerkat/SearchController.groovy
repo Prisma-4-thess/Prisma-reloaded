4690ee4d9ba59f777667ccb2962d2ca3d8421646
spirosbond <spirosbond@hotmail.com>
2014-12-18 22:45:42 +0100

    number of total results is got by totalCount variable.

--- grails-app/controllers/com/meerkat/SearchController.groovy
+++ grails-app/controllers/com/meerkat/SearchController.groovy
@@ -16,38 +16,50 @@ class SearchController {
             params.offset = getGrailsApplication().getConfig().pagination.defaultOffset
         }
 
-        List<Decision> decisionList
-        List<Signer> signerList
-        List<Type> typeList
-        List<Geo> geoList
+        String searchQuery = params.searchBarQuery;
+        if(searchQuery==null) searchQuery = " " //TODO: Should be removed
+        def decisionList
+        def signerList
+        def typeList
+        def geoList
         int numOfResultsDec = 0, numOfResultsSigner = 0, numOfResultsType = 0, numOfResultsGeo = 0
 
         //TODO: Call service for sort/paginate
         if (params.clicked == "decision") {
             decisionList = Decision.list(params)
+            numOfResultsDec = decisionList.totalCount
         } else if (params.clicked == "signer") {
             signerList = Signer.list(params)
+            numOfResultsSigner = signerList.totalCount
         } else if (params.clicked == "type") {
             typeList = Type.list(params)
+            numOfResultsType = typeList.totalCount
         } else if (params.clicked == "geo") {
             geoList = Geo.list(params)
+            numOfResultsGeo = geoList.totalCount
+//            geoList = SearchService.searchForPOI(searchQuery,params.max,params.offset,params.sort,params.order)
         } else {
             //TODO: Call service for search
             if (decisionList == null) {
                 decisionList = Decision.list(params)
-                numOfResultsDec = Decision.getAll().size()
+//                numOfResultsDec = Decision.getAll().size()
+                numOfResultsDec = decisionList.totalCount
             }
             if (signerList == null) {
                 signerList = Signer.list(params)
-                numOfResultsSigner = Signer.getAll().size()
+//                numOfResultsSigner = Signer.getAll().size()
+                numOfResultsSigner = signerList.totalCount
             }
             if (typeList == null) {
                 typeList = Type.list(params)
-                numOfResultsType = Type.getAll().size()
+//                numOfResultsType = Type.getAll().size()
+                numOfResultsType = typeList.totalCount
             }
             if (geoList == null) {
                 geoList = Geo.list(params)
-                numOfResultsGeo = Geo.getAll().size()
+                //numOfResultsGeo = Geo.getAll().size()
+                //geoList = SearchService.searchForPOI(searchQuery,params.max,params.offset,params.sort,params.order)
+                numOfResultsGeo = geoList.totalCount
             }
         }
 
