f0b9fe84f7eb85889614bb59a3a64183fff268e1
plutoattack <plutoattackme@gmail.com>
2014-12-28 21:49:56 +0200

    code for geodefining--incomplete

--- grails-app/controllers/com/meerkat/LuceneController.groovy
+++ grails-app/controllers/com/meerkat/LuceneController.groovy
@@ -3,5 +3,7 @@ package com.meerkat
 class LuceneController {
 
     def index() {
+        float score=0.4
+        LuceneService.decision2geo(score)
     }
 }
--- grails-app/services/com/meerkat/LuceneService.groovy
+++ grails-app/services/com/meerkat/LuceneService.groovy
@@ -1,17 +1,62 @@
 package com.meerkat
 
 import grails.transaction.Transactional
+import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.el.GreekAnalyzer
+import org.apache.lucene.document.Document
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.Query
+import org.apache.lucene.search.ScoreDoc
+import org.apache.lucene.store.Directory
+import org.apache.lucene.store.FSDirectory
 
 @Transactional
 class LuceneService {
 
-    static def decision2geo(Geo geo) {
+    static def decision2geo(float score) {
+        def decisions=Decision.all
+        decisions.each { d ->
+            if (d.subject) {
+                def subject = SearchService.Stem(d.subject)
+                def retList = indexSearch(subject, "/tmp/geoindex", score)
+                if (!retList.isEmpty()) {
+                    def command = "echo \"" + d.subject + retList + "\"" + " >log"// Create the String
+                    println(command)
+                    def proc = command.execute()                 // Call *execute* on the string
+                    proc.waitFor()                               // Wait for the command to finish
 
-        def decisions=SearchService.searchForDecisions(geo.namegrk,100,0,null,null)
-        decisions.each{d->
-            if(!geo.decisions.contains(d)){
-                geo.addToDecisions(d)
+//Obtain status and output
+                    println "return code: ${proc.exitValue()}"
+                    println "stderr: ${proc.err.text}"
+                    println "stdout: ${proc.in.text}" // *out* from the external program is *in* for groovy
+                }
             }
         }
+    }
+    static def indexSearch(String param,String index_directory,float score){
+        def returnList=[]
+        Analyzer analyzer=new GreekAnalyzer()
+        File fsdFile=new File(index_directory)
+        Directory directory = FSDirectory.open(fsdFile);
+        DirectoryReader ireader = DirectoryReader.open(directory);
+        IndexSearcher isearcher = new IndexSearcher(ireader);
+        // Parse a simple query that searches for "text":
+        QueryParser parser = new QueryParser("stemed", analyzer);
+        Query query = parser.parse(param)
+        ScoreDoc[] hits = isearcher.search(query, null, 100).scoreDocs;
+//        println hits.toString()
+        // Iterate through the results:
+        for (int i = 0; i < hits.length; i++) {
+            if(hits[i].score>score){
+                Document hitDoc = isearcher.doc(hits[i].doc);
+                def map=[prototype:hitDoc.get("prototype"),score:hits[i].score]
+                returnList.add(map)
+            }
+        }
+        ireader.close();
+        directory.close()
+        return returnList
     }
 }
